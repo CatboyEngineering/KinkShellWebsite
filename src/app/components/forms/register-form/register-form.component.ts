1cf5e555ba8e0262b6ab527abb5f703289363ba9
Kenji <>
2024-04-01 21:48:13 -0600

    Make the design way better

--- src/app/components/forms/register-form/register-form.component.ts
+++ src/app/components/forms/register-form/register-form.component.ts
@@ -23,15 +23,19 @@ export class RegisterFormComponent {
     this.registerForm = this.formBuilder.group<RegisterForm>({
       username: this.formBuilder.nonNullable.control('', {
         updateOn: 'submit',
-        validators: Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9]{6,}$')])
+        validators: Validators.compose([Validators.required, Validators.minLength(6), Validators.pattern('^[a-zA-Z0-9]+$')])
       }),
       password: this.formBuilder.nonNullable.control('', {
         updateOn: 'submit',
-        validators: Validators.compose([Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')])
+        validators: Validators.compose([Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).+$')])
+      }),
+      confirmPassword: this.formBuilder.nonNullable.control('', {
+        updateOn: 'submit',
+        validators: Validators.required
       }),
       displayName: this.formBuilder.nonNullable.control('', {
         updateOn: 'submit',
-        validators: Validators.compose([Validators.required, Validators.pattern(/^[a-zA-Z0-9\s\-\'_]{8,32}$/)])
+        validators: Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(32), Validators.pattern(/^[a-zA-Z0-9\s\-\'_]+$/)])
       }),
       betaPassword: this.formBuilder.nonNullable.control('', { updateOn: 'submit', validators: Validators.required })
     });
@@ -41,6 +45,15 @@ export class RegisterFormComponent {
     this.appDetailsStateService.onFormErrorsCleared();
 
     if (this.registerForm.valid) {
+      if(this.registerForm.controls.password.value !== this.registerForm.controls.confirmPassword.value) {
+        this.appDetailsStateService.onFormError({
+          form: FormName.REGISTER_ACCOUNT,
+          error: "Passwords do not match!"
+        });
+
+        return;
+      }
+
       let request: AccountCreateRequest = {
         username: this.registerForm.controls.username.value.toLowerCase(),
         password: this.registerForm.controls.password.value,
--- src/app/components/ui/ui-form-field-error/ui-form-field-error.component.ts
+++ src/app/components/ui/ui-form-field-error/ui-form-field-error.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, ControlContainer, FormGroup, FormGroupDirective, NgForm, UntypedFormGroup } from '@angular/forms';
-import { Observable, map } from 'rxjs';
+import { Observable, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'ui-form-field-error [form][errorFor]',
@@ -11,51 +11,16 @@ import { Observable, map } from 'rxjs';
   styleUrl: './ui-form-field-error.component.css'
 })
 export class UiFormFieldErrorComponent implements OnInit {
-  @Input() errorFor: string | undefined;
-  @Input() form: FormGroupDirective | undefined;
+  @Input({required: true}) errorFor!: string;
+  @Input({required: true}) errorType!: string;
+  @Input({required: true}) form!: FormGroupDirective;
 
-  errorMessages$: Observable<string[]> | undefined;
-  control: AbstractControl | undefined;
+  hasError$: Observable<boolean | undefined> | undefined;
 
   constructor() {}
 
   ngOnInit(): void {
-    let parentForm = this.form;
-    this.control = parentForm?.control.controls[this.errorFor!];
-    this.errorMessages$ = this.form?.ngSubmit.pipe(map(() => this.mapErrorMessage()));
-  }
-
-  private mapErrorMessage(): string[] {
-    var errors = [];
-
-    if (this.control?.hasError('required')) {
-      errors.push('This field is required');
-    }
-
-    if (this.control?.hasError('email')) {
-      errors.push('Please enter a valid email address');
-    }
-
-    if (this.control?.hasError('passwordStrength')) {
-      errors.push('Passwords must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character');
-    }
-
-    if (this.control?.hasError('confirmPassword')) {
-      errors.push('Passwords do not match');
-    }
-
-    if (this.control?.hasError('pattern')) {
-      errors.push('This field does not follow the pattern.');
-    }
-
-    if (this.control?.hasError('minlength')) {
-      if (this.errorFor === 'username') {
-        errors.push('Username must be at least 6 characters long');
-      } else if (this.errorFor === 'password') {
-        errors.push('Passwords must be at least 8 characters long');
-      }
-    }
-
-    return errors;
+    var control = this.form.form.controls[this.errorFor];
+    this.hasError$ = this.form!.ngSubmit.pipe(map(() => control.hasError(this.errorType)));
   }
 }
